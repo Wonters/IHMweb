2aed69447cb4c99533b79e6614130e1926bef08e
Loic Chagnoleau <loic.chagnoleau@sigfox.com>
2018-02-21 11:23:15 +0100

    complete some functions

--- acbbs/testcases/rxGainLNAs.py
+++ acbbs/testcases/rxGainLNAs.py
@@ -5,16 +5,15 @@ from acbbs.drivers.dut import *
 from acbbs.drivers.ate.SpecAn import *
 from acbbs.drivers.ate.RFSigGen import *
 
-import time
 from os.path import basename, splitext
 
 class rxGainLNAs(baseTestCase):
     def __init__(self):
         baseTestCase.__init__(self)
-        self.db = dataBase(name = "{0}_{1}".format(splitext(basename(__file__))[0], time.strftime("%Y_%m_%d_%H:%M:%S")))
+        self.db = dataBase(collection = splitext(basename(__file__))[0])
 
     def run(self):
         pass
 
     def test(self):
-        pass
+        self.db.writeDataBase(pmax=10, sensi=42, courant=0.25)
--- acbbs/tools/dataBase.py
+++ acbbs/tools/dataBase.py
@@ -4,6 +4,9 @@ from acbbs.tools.log import *
 from acbbs.tools.configurationFile import *
 
 from os.path import basename, splitext
+from pymongo import MongoClient
+
+import time
 
 class dataBase(object):
 
@@ -11,7 +14,7 @@ class dataBase(object):
         def __init__(self):
             return
 
-    def __init__(self, name = None, simulate = False):
+    def __init__(self, collection = None, simulate = False):
         self.logger = get_logger(splitext(basename(__file__))[0])
         if simulate :
             self.logger.debug("Init dataBase in Simulate")
@@ -19,17 +22,17 @@ class dataBase(object):
             self.logger.debug("Init dataBase")
 
         self.conf = configurationFile(file = splitext(basename(__file__))[0])
+        self.collection = "{0}_{1}".format(collection, time.strftime("%Y_%m_%d_%H_%M_%S"))
+
         self.__openDataBase()
+        self.__createCollection()
 
     def writeDataBase(self, **kwargs):
-        """
-
-
-        @param  _kwargs :
-        @return  :
-        @author
-        """
-        pass
+        for key in kwargs:
+            self.logger.debug("Write in database : {0} \"{1}\":\"{2}\"".format(self.collection, key, kwargs[key]))
+            post = {key:kwargs[key]}
+            post_id = self.db_collection.insert_one(post).inserted_id
+            self.logger.debug("successful writing at id : {0}".format(post_id))
 
     def readDataBase(self, id = None, param = None):
         """
@@ -43,6 +46,20 @@ class dataBase(object):
         pass
 
     def __openDataBase(self):
+        #get server, port and database from json configuration file
         server = self.conf.getConfiguration("mongodb-ip")
         port = self.conf.getConfiguration("mongodb-port")
-        self.logger.debug("Open MongoDB at : {0}:{1}".format(server, port))
+        database = self.conf.getConfiguration("mongodb-database")
+        self.logger.debug("Open MongoDB database \"{0}\" at : {1}:{2}".format(database, server, port))
+
+        #open MongoDB server
+        self.client = MongoClient(server, int(port))
+
+        #open MongoDB database
+        self.db = self.client[database]
+
+    def __createCollection(self):
+        self.logger.debug("Create collection {0}".format(self.collection))
+
+        #create collection
+        self.db_collection = self.db[self.collection]
